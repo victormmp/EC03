6992df9dd676d98bbba4477024d581e406d45c7a
victormmp <victormmp@outlook.com>
2019-11-11 18:14:25 -0300

    Fix sample

--- coleta_dados.R
+++ coleta_dados.R
@@ -23,12 +23,12 @@ count.dim <- 1
 for (d in dimensions){
 
     dim <- ceil(d)
-    
+
     for (r in 1:10) {
-        
-    
+
+
         cat("\nBuilding dimension ", dim)
-    
+
         fn <- function(X){
             if(!is.matrix(X)) X <- matrix(X, nrow = 1) # <- if a single vector is passed as X
             Y <- apply(X, MARGIN = 1,
@@ -39,14 +39,14 @@ for (d in dimensions){
         stopcrit <- list(names = "stop_maxeval", maxevals = 5000 * dim, maxiter = 100 * dim)
         probpars <- list(name = "fn", xmin = rep(-5, dim), xmax = rep(10, dim))
         popsize = 5 * dim
-    
+
         ## Config 1
         recpars1 <- list(name = "recombination_arith")
         mutpars1 <- list(name = "mutation_rand", f = 4)
         ## Config 2
         recpars2 <- list(name = "recombination_bin", cr = 0.7)
         mutpars2 <- list(name = "mutation_best", f = 3)
-    
+
         out <- ExpDE(mutpars = mutpars1,
                      recpars = recpars1,
                      popsize = popsize,
@@ -56,7 +56,7 @@ for (d in dimensions){
                      showpars = list(show.iters = "dots", showevery = 20))
         de <- list("dim"=dim, "best"=out$Fbest, "repetition"=r)
         out_dim.conf1 <- rbind(out_dim.conf1,de, stringsAsFactors=FALSE)
-    
+
         out <- ExpDE(mutpars = mutpars2,
                      recpars = recpars2,
                      popsize = popsize,
@@ -66,7 +66,7 @@ for (d in dimensions){
                      showpars = list(show.iters = "dots", showevery = 20))
         de <- list("dim"=dim, "best"=out$Fbest, "repetition"=r)
         out_dim.conf2 <- rbind(out_dim.conf2,de, stringsAsFactors=FALSE)
-    
+
     }
 
     count.dim <- count.dim + 1
